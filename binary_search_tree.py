fca3539cf057f5719f19799fd025ab01e6e19354
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-05 18:26:51 +0530

    push to repo

---
+++ binary_search_tree.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.leftChild = None
+        self.rightChild = None        
+    
+    def insert(self, data):
+        if self.value == data:
+            return False
+        else:
+            if self.value > data:
+                if self.leftChild:
+                    return self.leftChild.insert(data)
+                else:
+                    self.leftChild = Node(data)                                   
+                    return True
+            else:
+                if self.rightChild:
+                    return self.rightChild.insert(data)
+                else:
+                    self.rightChild = Node(data)
+                    return True
+
+    def find(self, data):
+        if (self.value == data):
+            return True
+        elif self.value > data:
+            if self.leftChild:
+                return self.leftChild.find(data)
+            else:
+                return False
+        else:
+            if self.rightChild:
+                return self.rightChild.find(data)
+            else:
+                return False
+
+    def lefttraversal(self):
+        if self:
+            print(self.value)            
+            if self.leftChild:
+                self.leftChild.lefttraversal()
+
+    def righttraversal(self):
+        if self:               
+            print(self.value)   
+            if self.rightChild:
+                self.rightChild.righttraversal()
+
+class Tree:
+
+    def __init__(self):
+
+        self.root = None
+    
+    def insert(self, data):
+        if self.root:
+            return self.root.insert(data)
+        else:            
+            self.root = Node(data)                                      
+            return True
+    
+    def find(self, data):
+        if self.root:
+            return self.root.find(data)
+        else:
+            return False
+
+    def lefttraversal(self):
+        return self.root.lefttraversal()
+
+    def righttraversal(self):
+        return self.root.righttraversal()
+
+bst = Tree()
+print(bst.insert(10))
+print(bst.insert(15))
+print(bst.insert(8))
+print(bst.insert(6))
+print(bst.lefttraversal())
+print(bst.righttraversal())
---
+++ linked_list.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+   
+class linkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next        
+
+
+    def find_next_node(self, data):
+        find_node = self.head                
+        if find_node.next.data == data:
+            return find_node.data , find_node.next
+
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return True
+        
+        last_node = self.head        
+        while last_node.next:                                                 
+            last_node = last_node.next             
+        last_node.next = new_node            
+
+
+    def insert_after_node(self, pre_node, data):
+        _ , previous_node = self.find_next_node(pre_node)        
+        inserted_node = Node(data)
+        inserted_node.next = previous_node.next        
+        previous_node.next = inserted_node
+        return True
+    """"
+        if not pre_node:
+            return False
+        else:
+            new_node = Node(data)
+            new_node.next = pre_node.next
+            pre_node.next = new_node
+    """
+
+    def delete_a_node(self, data):
+        node_to_delete = Node(data)
+        if node_to_delete is None:
+            return False
+        else:
+            cur_node = self.head  
+            prev_node = self.head                  
+            while cur_node.next:
+                if cur_node.data == data:                    
+                    prev_node.next = cur_node.next
+                    cur_node.next = None
+                else:
+                    prev_node = cur_node
+                    cur_node = cur_node.next
+
+
+sll = linkedList()        
+sll.append(3)
+sll.append(5)
+sll.append(6)
+sll.prepend(4)
+sll.insert_after_node(3,8)
+sll.print_list()
+sll.delete_a_node(5)
+sll.print_list()

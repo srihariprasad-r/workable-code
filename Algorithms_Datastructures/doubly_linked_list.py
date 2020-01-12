ef7a4ee87dc24721bb5c8e952357009eda582959
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-13 01:33:57 +0530

    added after node addition function to doubly linked list file

--- Algorithms_Datastructures/doubly_linked_list.py
+++ Algorithms_Datastructures/doubly_linked_list.py
@@ -46,6 +46,21 @@ class doubly_linked_list:
             new_node.prev = last_node
             new_node.next = None
         
+    
+    def add_after_node(self, key, data):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.next is None and cur_node.data == key:       #check if key is in head and there is only one element, call append
+                self.append(data)
+            elif cur_node.data == key:
+                new_node = Node(data)
+                next_node = cur_node.next
+                cur_node.next = new_node
+                next_node.prev = new_node
+                new_node.next = next_node
+                new_node.prev = cur_node
+            cur_node = cur_node.next
+                                        
 
 dll = doubly_linked_list()
 dll.append(1)
@@ -56,4 +71,6 @@ dll.append(4)
 #added prepend function
 dll.prepend(5)
 dll.prepend(9)
+#dll.print_linked_list()
+dll.add_after_node(1,30)
 dll.print_linked_list()

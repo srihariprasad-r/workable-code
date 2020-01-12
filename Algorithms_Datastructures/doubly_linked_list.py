04a6485b5141f5d87aec4ba29cca7d9205780d4c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-12 20:02:43 +0530

    added initial version for doubly linked list

---
+++ Algorithms_Datastructures/doubly_linked_list.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+    
+class doubly_linked_list:
+    def __init__(self):
+        self.head = None
+    
+    def print_linked_list(self):
+        cur_node = self.head        
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next        
+
+    def append(self, data):        
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+            #print("added new node:", new_node)
+            new_node.prev = None
+            new_node.next = None
+        else:
+            new_node = Node(data)
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            
+            last_node.next = new_node
+            new_node.prev = last_node
+            new_node.next = None
+        
+
+dll = doubly_linked_list()
+dll.append(1)
+dll.append(2)
+dll.append(3)
+dll.append(4)
+dll.print_linked_list()

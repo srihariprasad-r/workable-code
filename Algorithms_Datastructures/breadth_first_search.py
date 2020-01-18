47cbfc1e85f8d2fba438391b44424d49a21288af
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-19 00:38:28 +0530

    initial version of BFS

--- Algorithms_Datastructures/breadth_first_search.py
+++ Algorithms_Datastructures/breadth_first_search.py
@@ -1,1 +1,26 @@
-#sample code here
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+        self.top = None
+        self.bottom = None
+
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, data, top = None, bottom = None, left = None, right = None):
+        if self.data == Node(data):
+            return 
+        else:
+            self.data = Node(data)
+            if self.top and self.right and self.left and self.bottom:
+                self.data.top = Node(top)
+                self.data.left = Node(left)
+                self.data.right = Node(right)
+                self.data.bottom = Node(bottom)                                                
+            
+
+

7d8d6c58a087c8d9e766103ebc286d2c580cfab2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-19 22:48:15 +0530

    working version of BFS puzzle

--- Algorithms_Datastructures/breadth_first_search.py
+++ Algorithms_Datastructures/breadth_first_search.py
@@ -37,6 +37,8 @@ class Graph:
         self.visited = []
         self.current = None
         self.frontier = []
+        self.previous_value = None
+        self.backtrack = {}
 
     def add_node_graph(self, data, top = None, bottom = None, left = None, right = None):
         self.data = Node(data)
@@ -50,34 +52,29 @@ class Graph:
                 return v
 
     def breadth_first_search(self):
-        start_value = min(k for k in self.graph.keys() if k == 3)
-        print(start_value)
+        start_value = 1        
         self.frontier.append(start_value)     
-        while len(self.frontier) > 0:
-            self.current = self.frontier[0]    
-            print("current value:", self.current)           
+        while self.frontier:
+            self.current = self.frontier[0]                     
             node_edges = self.print_node_edges(self.frontier[0])        
-            while node_edges['left']:
+            while node_edges['left'] and node_edges['left'] not in self.visited:
                 self.frontier.append(node_edges['left'])                    
                 break
-            while node_edges['right']:
+            while node_edges['right'] and node_edges['right'] not in self.visited:
                 self.frontier.append(node_edges['right'])                   
                 break                 
-            while node_edges['top']:
+            while node_edges['top'] and node_edges['top'] not in self.visited:
                 self.frontier.append(node_edges['top'])                   
                 break
-            while node_edges['bottom']:
+            while node_edges['bottom'] and node_edges['bottom'] not in self.visited:
                 self.frontier.append(node_edges['bottom'])                                                           
-                break
-            print("frontier:", self.frontier)
-            self.visited.append(self.current)
-            print(self.visited)
+                break                                           
+            self.visited.append(self.current)                     
             self.frontier.remove(self.current)
-            print("after removal:", self.frontier)
-            break
-        
+        return self.visited
+
 g = Graph()
-g.add_node_graph(1)
+g.add_node_graph(1, right = 2)
 g.add_node_graph(2, left = 1 , right = 3 )
 g.add_node_graph(3, left = 2 , right = 4, top = 5, bottom = 7)
 g.add_node_graph(4, left = 3)
@@ -87,7 +84,7 @@ g.add_node_graph(7, bottom = 8 ,  top = 3)
 g.add_node_graph(8, top = 7 ,  left = 9, right = 10)
 g.add_node_graph(9, right = 8)
 g.add_node_graph(10, left = 8)
-print(g.print_node_edges(3))
-g.breadth_first_search()
+g.print_node_edges(3)
+print(g.breadth_first_search())
 
 

df90718e2deb55e7f185a75e50b6063e5f2dddb0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-01-19 21:43:16 +0530

    added dictionary to merge all nodes for BFS

--- Algorithms_Datastructures/breadth_first_search.py
+++ Algorithms_Datastructures/breadth_first_search.py
@@ -15,39 +15,53 @@ class Node:
         self.left = None
         self.top = None
         self.bottom = None
-
-
-class Graph:
-    def __init__(self):
-        self.graph = {}
-        self.data = None
+        self.nodes = {}
 
     def add_node(self, data, top = None, bottom = None, left = None, right = None):
         self.data = Node(data)
-        print("node added:", self.data.data)
         self.data.left = left
         self.data.right = right
         self.data.top  = top
-        self.data.bottom = bottom                                     
-        return True
+        self.data.bottom = bottom  
+        self.nodes['left'] = self.data.left
+        self.nodes['right'] = self.data.right        
+        self.nodes['top'] = self.data.top                
+        self.nodes['bottom'] = self.data.bottom            
+        return self.nodes
 
+class Graph:
+
+    def __init__(self):
+        self.graph = {}        
+        self.nodes = {}
+        self.visited = []
+        self.current = None
+        self.frontier = []
+
+    def add_node_graph(self, data, top = None, bottom = None, left = None, right = None):
+        self.data = Node(data)
+        self.nodes = self.data.add_node(data, top, bottom, left, right)                                  
+        self.graph[data] =  self.nodes
+        return True
+        
     def print_node_edges(self, data):
-        print("left node of {0} is : {1}".format(self.data.data, self.data.left))
-        print("right node of {0} is : {1}".format(self.data.data, self.data.right))
-        print("top node of {0} is : {1}".format(self.data.data, self.data.top))
-        print("bottom node of {0} is : {1}".format(self.data.data, self.data.bottom))                        
+        for k,v in self.graph.items():
+            if k == data:
+                return v
+
             
 g = Graph()
-print(g.add_node(1))
-print(g.add_node(2, left = 1 , right = 3 ))
-print(g.add_node(3, left = 2 , right = 4, top = 5, bottom = 7 ))
-print(g.add_node(4, left = 3))
-print(g.add_node(5, bottom = 3, top = 6 ))
-print(g.add_node(6, bottom = 5))
-print(g.add_node(7, bottom = 8 ,  top = 3))
-print(g.add_node(8, top = 7 ,  left = 9, right = 10))
-print(g.add_node(9, right = 8))
-print(g.add_node(10, left = 8))
-g.print_node_edges(10)
+g.add_node_graph(1)
+g.add_node_graph(2, left = 1 , right = 3 )
+g.add_node_graph(3, left = 2 , right = 4, top = 5, bottom = 7)
+g.add_node_graph(4, left = 3)
+g.add_node_graph(5, bottom = 3, top = 6 )
+g.add_node_graph(6, bottom = 5)
+g.add_node_graph(7, bottom = 8 ,  top = 3)
+g.add_node_graph(8, top = 7 ,  left = 9, right = 10)
+g.add_node_graph(9, right = 8)
+g.add_node_graph(10, left = 8)
+print(g.print_node_edges(3))
+#g.return_edges(3)
 
 

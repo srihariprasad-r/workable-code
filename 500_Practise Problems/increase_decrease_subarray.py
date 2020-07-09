d084d1690cb14c03162333eb7c7902436406af37
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-10 01:13:21 +0530

    needs rework

--- 500_Practise Problems/increase_decrease_subarray.py
+++ 500_Practise Problems/increase_decrease_subarray.py
@@ -6,24 +6,31 @@ Output: 3, 0 , -1
 """
 
 def decreaseincreasearray(x, k):
-    inc_count = 0
-    dec_count = 0
+    inc_lis = [1] * k
+    dec_lis = [1] * k
+    same_lis = [1] * k
+    window_dict = {}
+    inc_count= 0 
+    dec_count= 0 
     same_count = 0
-    inc_dec_dict = {}    
+
     for i in range(0, len(x)):
-        y = x[i:i+k]
-        for j in range(len(y)-1):
-            if y[j] < y[j + 1]:
-                inc_count += 1
-                inc_dec_dict.setdefault('inc', []).append(i)
-            elif y[j] > y[j + 1]:
-                dec_count -= 1
-                inc_dec_dict.setdefault('dec', []).append(i)
-            else:
-                same_count = 1
-                inc_dec_dict.setdefault('same', []).append(i)
+        window_dict[i] = x[i:i+k]
+
+    print(window_dict)
+
+    for z , v in window_dict.items():
+        for i in range(len(v)):
+            for j in range(0, i):
+                if v[i] > v[j] and inc_lis[i] < inc_lis[j] + 1:
+                    inc_lis[i] += 1 
+                elif v[i] < v[j] and dec_lis[i] > dec_lis[j] - 1:
+                    dec_lis[i] -= 1                     
+            
+    print(inc_lis, dec_lis)
+    #print(window_dict)   
+    #print(inc_lis, dec_lis, same_lis)
     #print(inc_count, dec_count, same_count)
-    print(inc_dec_dict)
 
 
              

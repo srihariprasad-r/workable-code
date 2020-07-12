e4c33cfc4495ab8ed05c2b0d7ecbc8250f34486e
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-12 23:54:05 +0530

    added cses problem set 1

---
+++ cses/wierd_algorithm.py
@@ -0,0 +1,26 @@
+"""
+Consider an algorithm that takes as input a positive integer n. 
+If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
+The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3→10→5→16→8→4→2→1
+
+Input:
+3
+
+Output:
+3 10 5 16 8 4 2 1
+"""
+
+def wierdAlgorithm(n):
+    while(n > 1):
+        if (n%2 == 0):
+            n = n/2
+            print(int(n))
+        else:
+            n= 3*n+1
+            print(int(n))
+    
+    #print(n)
+
+n = 3
+wierdAlgorithm(n)

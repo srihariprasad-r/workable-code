411a7ef82cdb6fc9350539f8548498e2a8d6419c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-18 00:33:41 +0530

    added single window problem to find max using while loop

--- 500_Practise Problems/sliding_window_max_iteration.py
+++ 500_Practise Problems/sliding_window_max_iteration.py
@@ -2,7 +2,9 @@
 Given an array and an integer K, find the maximum for each and every contiguous subarray of size k
 """
 
-def slidingWindowMaxNum(A, k):
+#Method 1:
+
+def slidingWindowMaxNum1(A, k):
   for i in range(len(A)-k+1):    
     maxNum = A[i]
     #print(maxNum)
@@ -12,5 +14,32 @@ def slidingWindowMaxNum(A, k):
     print(maxNum, end=" ")
 
 
+#A = [1, 2, 3, 4]
+#slidingWindowMaxNum1(A, 3)
+
+
+#Method 2:
+
+def slidingWindowMaxNum2(A, k):
+  p = 0
+  n = len(A)
+  q = k - 1 
+  t = p
+  maxNum = A[k-1]
+
+  while(q <= n -1):
+    if(A[p] > maxNum):
+      maxNum = A[p]
+    p += 1
+    
+    if (q == p and p != n):
+      print(maxNum, end=" ")
+      q += 1
+      p = t + 1
+      t = p
+
+      if (q < n):
+        maxNum = A[q]
+
 A = [1, 2, 3, 4]
-slidingWindowMaxNum(A, 3)
+slidingWindowMaxNum2(A, 3)

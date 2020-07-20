cc74458034a37eab3cd87f3d742363cdab543801
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-20 21:26:03 +0530

    added solution for maximum continous subarray sum with subarray index range

--- 500_Practise Problems/max_subarray_contiguous_sum.py
+++ 500_Practise Problems/max_subarray_contiguous_sum.py
@@ -5,12 +5,25 @@ Given array of integers, find the maximum contiguous sum
 def maxContiguousSum(arr):
   maxSum = arr[0]
   maxContSum = arr[0]
-  index_list = []
-  for i in range(1, len(arr)):
-    maxSum = max(maxSum+arr[i], arr[i])
-    maxContSum = max(maxSum, maxContSum)
+  start = 0
+  end = 0
   
-  print(maxContSum)
+  for i in range(1, len(arr)):
+    #running sum
+    maxSum = max(maxSum+arr[i], arr[i])
+    #if sum is less than 0, shift pointer to next element
+    if maxSum <0 :
+      start = i+1      
+    elif maxSum > maxContSum:
+      end = i 
+    #reassign the correct max sum  
+    maxContSum = max(maxSum, maxContSum)    
+
+  return start, end, maxSum
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxContiguousSum(arr))
+
 
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 maxContiguousSum(arr)

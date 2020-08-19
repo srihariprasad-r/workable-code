0a44d2bbdf99fa6356ae62e475f9e1420d70da49
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-08-19 12:30:58 +0530

    added multiple patch mock for same testcase

--- unittest_debugging/mock_test.py
+++ unittest_debugging/mock_test.py
@@ -1,11 +1,11 @@
 import unittest
-from prd_code import add_multiply, division
+from prd_code import add_multiply, division, add_multiply_subtract
 from unittest.mock import Mock, patch
 
 class Simpletest(unittest.TestCase):
 
     @patch('prd_code.multiply')   
-    def test_add(self, mock_multiply):
+    def test_add_multiply(self, mock_multiply):
 
         x = 3
         y = 6
@@ -18,6 +18,19 @@ class Simpletest(unittest.TestCase):
         self.assertEqual(9, addition)
         self.assertEqual(18, multiply)
 
+
+    def test_add_multiply_subtract(self):     
+        with patch('prd_code.multiply') as mock_multiply:
+            with patch('prd_code.subtract') as mock_subtract:
+                mock_subtract.return_value = 3
+                addition, subtract = add_multiply_subtract(3, 6)
+                self.assertEqual(3, subtract) 
+                self.assertEqual(9, addition)                   
+                mock_multiply.return_value = 18
+                addition, multiply = add_multiply_subtract(6, 3)
+                self.assertEqual(18, multiply)                 
+                self.assertEqual(9, addition)                
+            
 
 if __name__ == '__main__':
     unittest.main()

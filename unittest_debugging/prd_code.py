c898c91f874968fb3d84c11144249e1bc8bb137c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-08-19 17:38:36 +0530

    added recent version of code

--- unittest_debugging/prd_code.py
+++ unittest_debugging/prd_code.py
@@ -4,6 +4,16 @@ def add_multiply(x, y):
     return add , multiple
 
 
+def add_multiply_subtract(x, y):
+    add = x + y
+    if x > y:
+        multiple = multiply(x , y)
+        return add, multiple
+    else:
+        difference = abs(subtract(x, y))
+        return add , difference
+
+
 def subtract(x, y):
     return x - y
 

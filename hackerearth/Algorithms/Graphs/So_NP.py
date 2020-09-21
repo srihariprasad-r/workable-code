c2ab3016b8e5c8d8d3e7b5d7d817ad735ec5c177
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-09-21 19:04:50 +0530

    fixed several errors with input data and uploaded the working version

--- hackerearth/Algorithms/Graphs/So_NP.py
+++ hackerearth/Algorithms/Graphs/So_NP.py
@@ -8,12 +8,16 @@ print('Hi, %s.' % name)         # Writing output to STDOUT
 '''
 
 # Write your code here
-import sys
-inputs = sys.stdin.read().splitlines()
+# import sys
+# inputs = sys.stdin.read().splitlines()
 
-n = int(inputs[0][0])
-m = int(inputs[0][2])
-k = int(inputs[0][4])
+arr = [int(x) for x in input().split()]
+
+n = int(arr[0])
+m = int(arr[1])
+k = int(arr[2])
+
+print(m)
 
 connected_component=0
 visited = [0] * (n+1)
@@ -27,13 +31,17 @@ def dfs(node):
                 dfs(k)
     return 
 
-for i in range(1, m+1):
-    adj_dict.setdefault(int(inputs[i][0]), []).append(int(inputs[i][2]))
-    adj_dict.setdefault(int(inputs[i][2]), []).append(int(inputs[i][0]))
+for i in range(m):
+    input_list = list(map(int,input().split()))    
+    adj_dict.setdefault(int(input_list[0]), []).append(int(input_list[1]))
+    adj_dict.setdefault(int(input_list[1]), []).append(int(input_list[0]))
 
-for i in range(1, n+1):
+for i in range(1,n+1):
     if not visited[i]:
         dfs(i)
         connected_component += 1
 
-print(m- n + connected_component)
+if connected_component > k:
+    print(-1)
+else:
+    print(m- n + k)        

580df0c9cbaef982ab0767893ae2de4b1bc53a0c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-09-21 20:01:54 +0530

    added solution for build a graph problem

---
+++ hackerearth/Algorithms/Graphs/Build_a_graph.py
@@ -0,0 +1,16 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+n = int(input())
+
+if n % 2 == 0 and n > 2:
+    print("Yes")
+else:
+    print("No")

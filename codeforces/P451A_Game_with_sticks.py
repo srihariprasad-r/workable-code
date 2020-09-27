f595bd03c6eec03719e8f6049fdbb17a6b50b227
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-09-27 18:54:14 +0530

    added solution for P451A

--- codeforces/P451A_Game_with_sticks.py
+++ codeforces/P451A_Game_with_sticks.py
@@ -3,9 +3,9 @@ arr = [int(x) for x in input().split()]
 n = arr[0]
 m = arr[1]
 
-if n == 1 or m == 1:
+min_stick = min(n,m)
+
+if min_stick %2 != 0:
     print("Akshat")
-elif (n* m) %2 == 0:
-    print("Malvika")
 else:
-    print("Akshat")
+    print("Malvika") 

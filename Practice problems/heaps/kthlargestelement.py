c43ef13e0302b85ef50ee982d0f9828040b70ef0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-12-26 12:51:24 +0530

    added solution for kth largest element

--- Practice problems/heaps/kthlargestelement.py
+++ Practice problems/heaps/kthlargestelement.py
@@ -1,31 +1,31 @@
-def heap_sort(arr):
-    build_heap(arr)
-    for i in range(len(arr) -1 , -1, -1):
+def heap_sort(arr, k):
+    build_heap(arr, k)
+    for i in range(len(arr)-1, len(arr)-k, -1):
         arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, len(arr)-1)
+        heapify(arr, 0, i)
 
-def build_heap(arr):
-    length = len(arr) -1
-    for i in range((len(arr)//2)-1, -1, -1):
-        heapify(arr,index=i, size=length)
+def build_heap(arr, k):
+    # length = len(arr)
+    for i in range((len(arr)//2) -1, -1, -1):
+        heapify(arr,index=i, size=len(arr))
 
 def heapify(arr, index, size):
     left = 2 * index + 1
     right = 2 * index + 2
 
-    if left <= size and arr[left] > arr[index]:
+    if left < size and arr[left] > arr[index]:
         largest = left
     else: 
         largest = index
     
-    if right <= size and arr[right] > arr[largest]:
+    if right < size and arr[right] > arr[largest]:
         largest = right
     
     if largest != index:
         arr[largest], arr[index] = arr[index], arr[largest]
         heapify(arr,largest,size)
     
-
+k = 4
 array =  [12, 11, 13, 5, 6, 7]
-heap_sort(array)
-print(array)
+heap_sort(array, k)
+print(array[0])

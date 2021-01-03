18b1dfd7a562b4febf6a92cd77f43d24a333da12
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-03 19:49:26 +0530

    added solution for first missing element in the array

--- Practice problems/arrays/firstmissingelement.py
+++ Practice problems/arrays/firstmissingelement.py
@@ -1,5 +1,16 @@
 def firstmissingelement(arr):
-    pass
+    arr.sort()
+    for i in range(1, len(arr)):
+        if arr[i] < 0 or arr[i] > len(arr):
+            continue
+        else:
+            if arr[i] != i + 1:
+                tmp = arr[arr[i] - 1]
+                arr[arr[i] - 1] = arr[i]
+                arr[i] = tmp
+        
+    return [i+1 for i in range(len(arr)) if arr[i] != i + 1][:1]
+
 
 arr = [-2, 11, -3, 1, 3, 4]
 print(firstmissingelement(arr))

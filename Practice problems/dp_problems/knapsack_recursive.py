291a9bb95a21ad9f822ca22b7d0256b60dcbd17e
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-12 23:45:45 +0530

    added recursive traceback

--- Practice problems/dp_problems/knapsack_recursive.py
+++ Practice problems/dp_problems/knapsack_recursive.py
@@ -1,3 +1,20 @@
+"""
+from functools import wraps
+
+def trace(func):
+    separator = '|  '
+    trace.recursion_depth = 0
+
+    @wraps(func)
+    def traced_func(*args, **kwargs):
+        trace.recursion_depth += 1
+        result = func(*args, **kwargs)
+        trace.recursion_depth -= 1
+        print(f'{separator * (trace.recursion_depth + 1)}|-- return {result}')
+        return result
+    return traced_func
+"""
+
 def knapsack_recursive(wt, val, w, n):
     if (n == 0 or w == 0):
         return 0
@@ -13,4 +30,5 @@ wt = [1, 2, 3, 5]
 val = [1, 4, 7, 10]
 w = 8
 n = len(wt)
+# result = trace(knapsack_recursive)
 print(knapsack_recursive(wt, val, w, n))

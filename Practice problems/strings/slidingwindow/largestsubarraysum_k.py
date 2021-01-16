719a296d8dd7bbded232c07b78d70259e18d2d58
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-16 19:09:03 +0530

    added solution for largest subarray with sum k

---
+++ Practice problems/strings/slidingwindow/largestsubarraysum_k.py
@@ -0,0 +1,19 @@
+def largestsubarraysum(arr,k):
+    rsum, mx, i, j = 0, 0, 0, 0
+    while j < len(arr):
+        rsum += arr[j]
+        while rsum > k:
+            rsum -= arr[i]
+            i += 1
+        if rsum == k:
+            mx = max(mx, j - i + 1)
+            j += 1
+        elif rsum < k:
+            j += 1
+
+    return mx
+
+
+arr = [4, 1, 1, 1, 2, 3, 2, 5
+k = 5 
+print(largestsubarraysum(arr, k))

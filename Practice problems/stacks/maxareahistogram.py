52ce5ede4c7e9680581c2d2da1cd8bcd2eaf1df1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-20 21:24:55 +0530

    updated solution to find maximum area of histogram

--- Practice problems/stacks/maxareahistogram.py
+++ Practice problems/stacks/maxareahistogram.py
@@ -1,6 +1,20 @@
+"""
+Find maximum area of histogram:
+1. Calculate NSL(nearest smallest left) for each element
+2. Calculate NSR(nearest smallest right) for each element
+3. For both calculations,
+    1. when current element is last(for NSR), take len(arr) as NSR(right side) since there is no building to compare
+    2. when current element is first(for NSL), take -1 as NSL(left side) since there is no building to compare
+4. From this, two arrays are derived and to find width of each element,
+    width[i] = right[i] - left[i] - 1
+we do reduce -1 as left and right are boundaries till we can find area and we should neglect the NSL/NSR as they are smaller than current element
+5. Multiply array element with width to get the area
+"""
+
 def maxareahistogram(arr):
     result_right = [0] * len(arr)
     result_left = [0] * len(arr)
+    width = [0] * len(arr)
     for i in range(len(arr)):
         stck_right = []
         stck_left = []
@@ -38,8 +52,10 @@ def maxareahistogram(arr):
         if len(stck_left) == 0:
             result_left[i] = -1
 
-    return result_left, result_right
+    for i in range(len(result_right)):
+        width[i] = result_right[i] - result_left[i] - 1
 
+    return [arr[i]*width[i] for i in range(len(arr))]
 
 arr = [6, 2, 5, 4, 5, 1, 6]
 print(maxareahistogram(arr))

e2351289573ccc8099d589e4b24c0fc615f0353f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-24 21:59:30 +0530

    updated solution for longest common subsequence

--- Practice problems/dp_problems/longestcommonsubsequence/lcs_memonized.py
+++ Practice problems/dp_problems/longestcommonsubsequence/lcs_memonized.py
@@ -2,17 +2,17 @@ def lcs_memonized(x, y, n, m):
     if n== 0 or m == 0:
         return 0
 
-    if dp[m][n] != -1:
-        return dp[m][n]
+    if dp[n-1][m-1] != -1:
+        return dp[n-1][m-1]
 
     if x[n-1] == y[m-1]:
-        dp[m][n] =  1 + lcs_memonized(x, y, n-1, m-1)
+        dp[n-1][m-1] =  1 + lcs_memonized(x, y, n-1, m-1)
     else:
-        dp[m][n] =  max(lcs_memonized(x, y, n, m-1), lcs_memonized(x, y, n-1, m))
+        dp[n-1][m-1] =  max(lcs_memonized(x, y, n, m-1), lcs_memonized(x, y, n-1, m))
 
-    return dp[m][n]
+    return dp[n-1][m-1]
 
 x = 'abcdegh'
 y = 'abcdfr'
-dp = [[-1 for i in range(len(x)+1)] for j in range(len(y)+1)]
+dp = [[-1 for i in range(len(y))] for j in range(len(x))]
 print(lcs_memonized(x,y, len(x), len(y)))

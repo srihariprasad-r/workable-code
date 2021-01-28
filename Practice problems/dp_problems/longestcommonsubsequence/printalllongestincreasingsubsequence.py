7d1abd9a2f7908a46c466c81a96be8165c47c779
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-01-28 23:54:46 +0530

    initial version for print all longest increasing subsequences

---
+++ Practice problems/dp_problems/longestcommonsubsequence/printalllongestincreasingsubsequence.py
@@ -0,0 +1,44 @@
+# incorrect solution
+
+from collections import deque
+
+q = deque()
+
+def printalllis(arr):
+    omax, oindex = 0, 0
+    dp = [0] * len(arr)
+
+    dp[0] = 1
+
+    for i in range(1, len(arr)):
+        mx, j = 0, 0 
+        while j < i:
+            if arr[j] < arr[i]:
+                if mx < dp[j]:
+                    mx  = dp[j]
+            j += 1
+        dp[i] =  mx + 1
+
+    for i in range(len(dp)):
+        if omax < dp[i]:
+            omax = dp[i]
+            oindex = i
+
+    q.append(oindex)
+    result = []
+    while len(q) > 0:
+        curList = []
+        val = q.popleft()
+        curList.append(arr[val])
+        for j in range(val-1, 0, -1):
+            if arr[val] > arr[j]:
+                q.append(j)
+                curList.append(arr[j])
+        result.append(curList)
+
+    print(result)
+            
+
+
+arr = [10, 22, 9, 33, 50, 41, 60, 80, 9]
+print(printalllis(arr))

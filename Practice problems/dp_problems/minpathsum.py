809efbeb10bee27b7cfc02efab274f14b0329f83
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-02-05 23:27:49 +0530

    fixed an issue with minpathsum

--- Practice problems/dp_problems/minpathsum.py
+++ Practice problems/dp_problems/minpathsum.py
@@ -2,49 +2,45 @@ from collections import deque
 
 q = deque()
 
-def mincostpath(arr):
-    dp = [[0 for i in range(len(arr))] for j in range(len(arr))]
+def mincostpath(grid):
+    dp = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
     
     dp[-1][-1] = 1
     
-    for i in range(len(arr)-1, -1, -1):
-        for j in range(len(arr)-1, -1, -1):
-            if i == len(arr)-1 : 
-                if j < len(arr)-1:
-                    dp[i][j] = dp[i][j+1] + arr[i][j]
-            elif j == len(arr)-1:
+    for i in range(len(grid)-1, -1, -1):
+        for j in range(len(grid[0])-1, -1, -1):
+            if i == len(grid)-1 : 
+                if j < len(grid[0])-1:
+                    dp[i][j] = dp[i][j+1] + grid[i][j]
+            elif j == len(arr[0])-1:
                 if i < len(arr)-1:
-                    dp[i][j] = dp[i+1][j] + arr[i][j]
+                    dp[i][j] = dp[i+1][j] + grid[i][j]
             else:
                 if dp[i+1][j] < dp[i][j+1]:
-                    dp[i][j] = arr[i][j] + dp[i+1][j]
+                    dp[i][j] = grid[i][j] + dp[i+1][j]
                 else:
-                    dp[i][j] = arr[i][j] + dp[i][j+1]
-    
-    q.append((0, 0, str(arr[0][0])))
-    result = []
+                    dp[i][j] = grid[i][j] + dp[i][j+1]
+                    
+    q.append((0, 0, str(grid[0][0])))
     
     while len(q) > 0:
-        el = q.pop()
+        el = q.popleft()
         i_index, j_index, strn = el[0], el[1], el[2]
         if i_index == len(dp) - 1 and j_index == len(dp[0]) -1:
-            result.append(strn)
+            return sum(int(i) for i in strn.split('->'))
         elif i_index == len(dp) - 1:
-            q.append((i_index, j_index+1, str(arr[i_index][j_index+1])+'->'+strn))
+            q.append((i_index, j_index+1, str(grid[i_index][j_index+1])+'->'+strn))
         elif j_index == len(dp[0]) - 1:
-            q.append((i_index+1, j_index, str(arr[i_index+1][j_index])+'->'+strn))
+            q.append((i_index+1, j_index, str(grid[i_index+1][j_index])+'->'+strn))
         else:
-            if dp[i_index+1][j_index] < dp[i_index][j_index+1]:
-                q.append((i_index+1, j_index, str(arr[i_index+1][j_index])+'->'+strn))
-            elif dp[i_index+1][j_index] > dp[i_index][j_index+1]:
-                q.append((i_index, j_index+1, str(arr[i_index][j_index+1])+'->'+strn))
+            if dp[i_index][j_index] + dp[i_index+1][j_index] < dp[i_index][j_index] + dp[i_index][j_index+1]:
+                q.append((i_index+1, j_index, str(grid[i_index+1][j_index])+'->'+strn))
+            elif dp[i_index][j_index] + dp[i_index+1][j_index] > dp[i_index][j_index] + dp[i_index][j_index+1]:
+                q.append((i_index, j_index+1, str(grid[i_index][j_index+1])+'->'+strn))
             else:
-                q.append((i_index+1, j_index, str(arr[i_index+1][j_index])+'->'+strn))
-                q.append((i_index, j_index+1, str(arr[i_index][j_index+1])+'->'+strn))
-                
-    return result  
-    
-
+                q.append((i_index+1, j_index, str(grid[i_index+1][j_index])+'->'+strn))
+                q.append((i_index, j_index+1, str(grid[i_index][j_index+1])+'->'+strn))
+ 
 arr = [
     [1,3,1],
     [1,5,1],

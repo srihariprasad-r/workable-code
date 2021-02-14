1ea0709be99f30057867cf97df140fbc87a973f1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-02-14 16:07:52 +0530

    added solution for star patterns

---
+++ Practice problems/foundation/starpattern_rightdiagonal.py
@@ -0,0 +1,25 @@
+"""
+          * 
+
+        *   
+
+      *     
+
+    *       
+
+  *
+"""
+def starpattern(n):
+    sp, st = n -1, 1
+    for i in range(n):
+        for _ in range(sp, -1, -1):
+            print(" ", end=" ")
+        for _ in range(st):
+            print("*", end=" ")
+        if sp < n - 1:
+            for _ in range(n-1-sp):
+                print(" ", end=" ")
+        print("\n")
+        sp -= 1
+        
+starpattern(5)

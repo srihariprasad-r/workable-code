c976b00cfdea3ca1e1fa88d2ad862dfdb7df28d4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-03-07 19:10:08 +0530

    added solution for stack

---
+++ Practice problems/foundation/stacks_queues/balancedbrackets.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+q = deque()
+
+def balancedbracket(str):
+
+    for i in range(len(str)):
+        if str[i] != ')':
+            q.append(str[i])
+        else:
+            if q[-1] == '(':
+                return True
+            else:
+                while q[-1] != '(':
+                    q.pop()
+                q.pop()
+
+    return False
+
+str = "(a+b)+((c+d))"
+print(balancedbracket(str))

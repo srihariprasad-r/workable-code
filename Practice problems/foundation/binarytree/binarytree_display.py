87b66229cffab1b1f9879d26049b5f75e3e238f6
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-03-14 11:19:53 +0530

    added binary tree for traversals

--- Practice problems/foundation/binarytree/binarytree_display.py
+++ Practice problems/foundation/binarytree/binarytree_display.py
@@ -1,3 +1,28 @@
+"""
+
+Pre-order : NLR (L-> left; N-> Node ; R -> Right)
+In-order : LNR
+Post-order: LRN
+
+        50
+       /  \
+      25   75
+     / \   /  \
+    12 37  62  87
+
+           N     N  L  R    N  L  R
+           N  |  L  L  L |  R  R  R
+Pre-order: 50 | 25 12 37 | 75 62 87
+
+            L  N  R   N     L  N  R
+            L  L  L | N  |  R  R  R
+In-order:  12 25 37 | 50 | 62 75 87
+
+
+            L  R  N    L  R  N  | N
+            L  L  L  | R  R  R  | N
+Post-order: 12 37 25 | 62 87 75 | 50
+"""
 class Node():
     def __init__(self, value, left=None, right=None):
         self.value = value

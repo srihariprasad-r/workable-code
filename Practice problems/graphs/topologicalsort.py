6e47ec4233107f9e302964630e6dc42b6be57931
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-05-01 23:58:50 +0530

    added solution for Kahn's algorithm with pq

--- Practice problems/graphs/topologicalsort.py
+++ Practice problems/graphs/topologicalsort.py
@@ -44,3 +44,51 @@ edges = [
         (6, 7)
     ]
 print(build_adjlist(edges, N))
+
+################################ Implementation example using Priority Queue ##################################################
+
+from queue import PriorityQueue
+
+pq = PriorityQueue()
+res = []
+
+def pq_build_adjlist(pq_edges, N):
+    pq_adjlist = {}
+    pq_inDegree = [0] * (N+1)
+
+    for src, dest in pq_edges:
+        pq_adjlist.setdefault(src,[]).append(dest) # this is directed graph, so only once we append incoming edges
+        pq_inDegree[dest] += 1
+
+    return pq_kahn_algorithm(pq_adjlist, N, pq_inDegree)
+
+def pq_kahn_algorithm(pq_adjlist, N, pq_inDegree):
+    for i in range(1, N+1):
+        if pq_inDegree[i] == 0:        # first collect all nodes which has 0 incoming edges(indegree=0)
+            pq.put(i)
+    
+    while not pq.empty():
+        el = pq.get()
+        res.append(el)
+
+        if el in pq_adjlist:
+            for child in pq_adjlist[el]:
+                pq_inDegree[child] -= 1        # for those child which has incoming edge from this popped out node, remove the edges
+                if pq_inDegree[child] == 0:
+                    pq.put(child)
+
+    return res
+
+N = 8
+pq_edges = [
+        (1, 4),
+        (1, 2),
+        (4, 2),
+        (4, 3),
+        (3, 2),
+        (5, 2),
+        (3, 5),
+        (8, 2),
+        (8, 6)
+    ]
+print(pq_build_adjlist(pq_edges, N))

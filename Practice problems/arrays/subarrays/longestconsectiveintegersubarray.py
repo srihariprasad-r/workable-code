0a6635d4c6045d28a6c15e2ef7f32ffe2f5b876b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-05-29 12:47:12 +0530

    updated solution for max subarray with consecutive integers

--- Practice problems/arrays/subarrays/longestconsectiveintegersubarray.py
+++ Practice problems/arrays/subarrays/longestconsectiveintegersubarray.py
@@ -1,42 +1,41 @@
-def longestconsectivesubarray(arr):
-    if len(arr) == 0:
-        return
-    elif len(arr) == 1:
-        return arr[0]
-    
+def maxlensubarrayconsective(arr):
     mx = -float('inf')
     mn = float('inf')
-    mxi = 0
-    mxj = 0
-    uq_set = set()    
-    i = 0
-    j = 0
+    mnidx = mxidx = 0
+    mxlen = 0
+    i = j = 0
+    unq_dct = set()
     
-    for c in arr:
-        uq_set.add(c)
-        
-        if j - i + 1 == len(uq_set):
-            for k in range(i, j +1):
-                if mx < arr[k]:
-                    mx = arr[k]
-                if mn > arr[k]:
-                    mn = arr[k]
-
-            if mx - mn + 1 == j - i + 1:
-                mxi = i
-                mxj = j
-
+    while j < len(arr):
+        unq_dct.add(arr[j])
+ 
+        if j - i + 1 < len(unq_dct):
             j += 1
-        elif j - i + 1 > len(uq_set):
-            while i == j:
-                uq_set.remove(arr[i])
-                i += 1
+        elif j - i + 1 > len(unq_dct):
+            if arr[i] in unq_dct:
+                unq_dct.remove(arr[i])
             i += 1
-            j += 1
         else:
-            j += 1
-                
-    return arr[mxi:mxj + 1]
+            mx = -float('inf')
+            mn = float('inf')
             
+            for k in range(i, j+1):
+                mx = max(mx, arr[k])
+                mn = min(mn, arr[k])
+ 
+            if mx  - mn + 1 == j - i + 1 and j - i + 1 > mxlen:
+                mnidx = i
+                mxidx = j
+                mxlen = j - i + 1
+            else:
+                unq_dct.remove(arr[i])
+                i += 1
+                
+            j += 1
+            
+
+    return arr[mnidx:mxidx+1]
+
+
 arr = [2, 5, 6, 2, 4, 3, 1, 0]
-print(longestconsectivesubarray(arr))
+print(maxlensubarrayconsective(arr))

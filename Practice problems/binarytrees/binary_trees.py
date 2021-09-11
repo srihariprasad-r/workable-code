7442fe9f9ffd5ccdfe59d9dc99d637103a3431a9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-09-11 23:49:55 +0530

    added binary tree traversals for inorder

--- Practice problems/binarytrees/binary_trees.py
+++ Practice problems/binarytrees/binary_trees.py
@@ -11,6 +11,12 @@ class BinaryTree:
     def print_nodes(self):
         return self.root.print_nodes()
 
+    def in_order_traversal(self):
+        return self.root.in_order_traversal()
+
+    def pre_order_traversal(self):
+        return self.root.pre_order_traversal()
+
 
 class Node(BinaryTree):
     def __init__(self, data, left=None, right=None):
@@ -24,12 +30,10 @@ class Node(BinaryTree):
             self.left = Node(data)
         elif not self.right:
             self.right = Node(data)
-        else:
-            if instruction:
-                if instruction == 'left':
-                    self.left = Node(data)
-                else:
-                    self.right = Node(data)
+        elif instruction == 'left':
+            self.left.insert_to_tree(data)
+        elif instruction == 'right':
+            self.right.insert_to_tree(data)
 
     def print_nodes(self):
         if self:
@@ -39,9 +43,32 @@ class Node(BinaryTree):
             if self.right:
                 self.right.print_nodes()
 
+    def in_order_traversal(self):
+        if self:
+            if self.left:
+                self.left.in_order_traversal()
+            print(self.data)
+            if self.right:
+                self.right.in_order_traversal()
+
+    def pre_order_traversal(self):
+        if self:
+            print(self.data)
+            if self.left:
+                self.left.pre_order_traversal()
+            if self.right:
+                self.right.pre_order_traversal()
+
 
 bt = BinaryTree()
 bt.insert_to_tree(2)
 bt.insert_to_tree(7)
-bt.insert_to_tree(6)
-bt.print_nodes()
+bt.insert_to_tree(5)
+bt.insert_to_tree(2, 'left')
+bt.insert_to_tree(6, 'right')
+bt.insert_to_tree(9, 'left')
+# bt.print_nodes()
+print('In-order traversal')
+bt.in_order_traversal()
+print('Pre order traversal')
+bt.pre_order_traversal()

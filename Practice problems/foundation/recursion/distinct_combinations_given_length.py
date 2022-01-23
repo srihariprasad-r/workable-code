f55ffa2f7edd0b9611cb8580cd206db318a2e255
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-01-23 19:05:23 +0530

    added solution for distinct combinations of sum equals to k

--- Practice problems/foundation/recursion/distinct_combinations_given_length.py
+++ Practice problems/foundation/recursion/distinct_combinations_given_length.py
@@ -19,3 +19,27 @@ def recursion(idx, n, k, ans=[], res=[]):
 n = 5
 k = 7
 print(recursion(1, n, k))
+
+
+# Method 2
+
+def recursion(n, k):
+    def solution(idx, n, k, ans=[], res=[]):
+        if idx > n or k < 0:
+            return
+
+        if k == 0:
+            res.append(ans)
+            return
+
+        for i in range(idx, n+1):
+            solution(i, n, k-i, ans + [i], res)
+
+        return res
+
+    return solution(1, n, k)
+
+
+n = 5
+k = 7
+print(recursion(n, k))

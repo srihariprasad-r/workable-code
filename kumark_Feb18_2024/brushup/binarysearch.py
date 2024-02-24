4197b28249450f16d99dfac0cebfeb16cc640113
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-02-24 22:30:52 +0530

    binary search - Kumar k

--- kumark_Feb18_2024/brushup/binarysearch.py
+++ kumark_Feb18_2024/brushup/binarysearch.py
@@ -26,3 +26,25 @@ def searcharray(arr,tgt):
     return -1
     
 print(searcharray(arr,tgt))
+
+# single element (wrong submission)
+
+arr = [3,3,7,7,10,11,11]
+
+def single_element(arr):
+    l = 0 
+    r = len(arr) - 1
+    
+    while l <= r:
+        mid = l + (r-l+1)//2
+
+        if arr[mid-1] != arr[mid] and arr[mid] != arr[mid+1]:
+            return mid
+        
+        if mid % 2:
+            l = mid + 1
+        else :
+            r = mid
+
+    
+print(single_element(arr))

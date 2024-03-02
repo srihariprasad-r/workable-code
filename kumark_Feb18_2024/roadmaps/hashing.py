cc97e8c9ce39f0af608eefa2c41c8bde93a0c5a4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-03 00:07:37 +0530

    hashing - Kumar k

--- kumark_Feb18_2024/roadmaps/hashing.py
+++ kumark_Feb18_2024/roadmaps/hashing.py
@@ -338,3 +338,25 @@ def solution(arr):
     return mx if mx != float('-inf') else -1
     
 print(solution(arr))
+
+# problem link: https://leetcode.com/problems/count-number-of-nice-subarrays/
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        s = 0
+        ans = 0
+
+        import collections
+        d = collections.defaultdict(int)
+
+        for c in nums:
+            if c % 2: s += 1
+
+            d[s] += 1
+
+            if s == k: 
+                ans += 1
+
+            if s - k in d:
+                ans += d[s-k]
+
+        return ans

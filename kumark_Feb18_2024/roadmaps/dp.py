4d1198a3614b04e82af953e737edd19908b820b2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-16 19:00:59 +0530

    dp - Kumar k

--- kumark_Feb18_2024/roadmaps/dp.py
+++ kumark_Feb18_2024/roadmaps/dp.py
@@ -38,3 +38,32 @@ def min_cost(dp, m):
     return dp[m] 
     
 print(min_cost(dp, m))
+
+# question link: https://www.desiqna.in/13822/google-dp-interview-question-jan-2023
+# arr = [5, 4, 2, 6]
+arr = [2 ,3 ,5, 8, 10]
+
+dp = [0] * len(arr)
+
+def oddsum(idx, s):
+    if idx < 0:
+        return 0
+    
+    if idx == 0:
+        s += arr[idx]
+        if s % 2 == 0: return 1
+        else: return 0
+    
+    # commented as it provides incorrect results
+    # if dp[idx] != 0: return dp[idx]
+    
+    odd_s_1 = 0
+    odd_s_2 = 0
+    
+    odd_s_1 = oddsum(idx-1, s + arr[idx])
+    odd_s_2 = oddsum(idx-2, s + arr[idx])
+    
+    dp[idx] = odd_s_1 + odd_s_2
+    return dp[idx]
+
+print(oddsum(len(arr)-1, 0))

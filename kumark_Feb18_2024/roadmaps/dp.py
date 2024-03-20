95cbe32f05c9b5653b10e691bd1ddbc455d8e5cb
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-20 20:01:12 +0530

    dp - Kumar K

--- kumark_Feb18_2024/roadmaps/dp.py
+++ kumark_Feb18_2024/roadmaps/dp.py
@@ -126,19 +126,27 @@ print(players(A, arr))
 
 
 # question link: https://leetcode.com/problems/palindromic-substrings/
-# wrong submission
-def countSubstrings(self, s: str) -> int:
+def countSubstrings(s):
     dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
     cnt = 0
-    
+    l = 1
     for i in range(len(s)):
-        for j in range(i, len(s)):
-            if i == j: dp[i][j] = 1
-    for i in range(len(s)):
-        for j in range(i,len(s)):
-            if s[i] != s[j]:
-                continue
-            else:
-                dp[i][j] = dp[i-1][j-1] + 1
+        dp[i][i] = 1
+        cnt += 1
+    i = 0
+    while i < len(s) - 1:
+        if s[i] == s[i+1]:
+            dp[i][i+1] = 1
+            cnt += 1
+        i += 1
+    l = 3
+    while l <= len(s):
+        i = 0
+        while i < len(s) - l + 1:
+            j = i + l - 1
+            if s[i] == s[j] and dp[i+1][j-1] == 1:
+                dp[i][j] =  1
                 cnt += 1
+            i += 1
+        l += 1
     return cnt

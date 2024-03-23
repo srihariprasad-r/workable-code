bb09fb31484b316886be6a0d7ae7184d0a38dadc
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-23 19:01:59 +0530

    dp - Kumar K

--- kumark_Feb18_2024/roadmaps/dp.py
+++ kumark_Feb18_2024/roadmaps/dp.py
@@ -189,3 +189,46 @@ def longestPalindrome(s):
     return mx
 
 print((longestPalindrome(s)))
+
+# question link: https://leetcode.com/problems/burst-balloons/submissions/904703007/
+# wrong submission
+def maxCoins(arr):
+    import copy 
+    visited = [0] * len(arr)
+    res = []
+    def permute(r):
+        if len(r) == len(arr):
+            res.append(copy.deepcopy(r))
+            return res
+            
+        for i in range(len(arr)):
+            if not visited[i]:
+                visited[i]= 1
+                r.append(i)
+                permute(r)
+                visited[i] = 0
+                r.pop()
+            
+        return res
+
+    orders = permute([])
+    mx = float('-inf')
+    for _, order in enumerate(orders):
+        m = 0
+        tmp = arr
+        bursted = [0] * len(arr)
+        for i in range(len(order)):
+            idx = order[i]
+            if idx == 0 :
+                m += ((tmp[idx]) * (tmp[idx+1] if bursted[idx+1] != 1 else 1))
+            elif idx == len(arr)-1:
+                m += ((tmp[idx-1] if bursted[idx-1] != 1 else 1) * (tmp[idx]))
+            else:
+                before = tmp[idx-1] if bursted[idx-1] != 1 else 1
+                at = tmp[idx] if bursted[idx] != 1 else 1
+                nxt = tmp[idx+1] if bursted[idx+1] != 1 else 1
+                m +=  (before * at  * nxt)
+            mx = max(mx, m)
+            bursted[idx] = 1
+            
+    return mx

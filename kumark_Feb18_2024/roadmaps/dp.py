95aa2cbac870fbd3f148ed2a09d23a3dcc5fb3cf
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-26 19:05:53 +0530

    dp - Kumar K

--- kumark_Feb18_2024/roadmaps/dp.py
+++ kumark_Feb18_2024/roadmaps/dp.py
@@ -272,25 +272,23 @@ print(max_deletes_same_sum(arr, dp))
 
 # question link: https://leetcode.com/contest/biweekly-contest-108/problems/partition-string-into-minimum-beautiful-substrings/
 # wrong submission
-class Solution:
-    def minimumBeautifulSubstrings(self, s: str) -> int:
-        powersOf5 = [format(5**i,'b')  for i in range(15)]
-        dp = [0] * len(s)
-
-        dp[0] = 1 if s[0] == '1' else 0
-
-        for i in range(1,len(s)):
-            j = i
-            t = s[j]
-            v = 0
-            while j > -1 :
-                if str(t) in powersOf5: 
-                    v = 1 + dp[j-1]
-                j -= 1
-                t += s[j]
-            dp[i] =  v
-        
-        return dp[len(s)-1] if dp[len(s)-1] > 0 else -1
+def minimumBeautifulSubstrings(s):
+    powersOf5 = [format(5**i,'b')  for i in range(15)]
+    dp = [0] * len(s)
+    dp[0] = 1 if s[0] == '1' else 0
+    for i in range(1,len(s)):
+        j = i
+        t = s[j]
+        if s[i] == '0': continue
+        v = float('inf')
+        while j > -1:
+            if str(t) in powersOf5:  
+                v = min(v, 1 + dp[j-1])
+            j -= 1
+            t += s[j]
+        dp[i] =  v
+    
+    return dp[len(s)-1] if dp[len(s)-1] != 0 else -1
 
 s = '111'
 print(minimumBeautifulSubstrings(s))

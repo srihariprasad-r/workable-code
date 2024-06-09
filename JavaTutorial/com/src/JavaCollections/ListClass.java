30a29d8202286e37d51027d3ca87d3af1bdaa34d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-06-09 15:33:57 +0530

    new class file

--- JavaTutorial/com/src/JavaCollections/ListClass.java
+++ JavaTutorial/com/src/JavaCollections/ListClass.java
@@ -1,4 +1,20 @@
-package JavaCollections;
+package com.src.JavaCollections;
+
+import java.util.*;
 
 public class ListClass {
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<String>();
+        arr.add("1");
+        arr.add("2");
+
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
+        }
+
+        Iterator<String> iterator = arr.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
 }

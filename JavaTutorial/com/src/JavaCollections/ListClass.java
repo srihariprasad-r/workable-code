9f16392b4f4ea5539ef14e24a819185581a68ee9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-06-09 22:28:26 +0530

    included arraylist methods

--- JavaTutorial/com/src/JavaCollections/ListClass.java
+++ JavaTutorial/com/src/JavaCollections/ListClass.java
@@ -6,18 +6,27 @@ import src.JavaCollections.ReverseListClass;
 public class ListClass {
     public static void main(String[] args) {
         List<String> arr = new ArrayList<String>();
+        arr.add("Seven");
         arr.add("One");
         arr.add("Two");
+        arr.add("Three");
+
+        System.out.println(arr.indexOf("Three")); //0
+        System.out.println(arr.lastIndexOf("Three")); //3
+
+        System.out.println(arr.containsAll(List.of("Three","Four"))); //false (all values should be in list1
+
+        System.out.println(arr.addAll(List.of("Five", "Six")));
 
         //for loop
         for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
+            System.out.println("for loop: " + " " + arr.get(i));
         }
 
         //Iterable
         Iterator<String> iterator = arr.iterator();
         while(iterator.hasNext()){
-            System.out.println(iterator.next());
+            System.out.println("iterator: " + " " + iterator.next());
         }
 
         //Reverse list through custom class
@@ -25,7 +34,7 @@ public class ListClass {
 
         Iterator<String> ril = rls.iterator();
             while (ril.hasNext()){
-                System.out.println(ril.next());
+                System.out.println("reversed: " + " " + ril.next());
             }
 
         }

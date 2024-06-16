3d64113131c39f34100ce1c6bad6c5c8395db596
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-06-16 22:04:30 +0530

    included LinkedList Class

--- JavaTutorial/com/src/JavaCollections/LinkedListClass.java
+++ JavaTutorial/com/src/JavaCollections/LinkedListClass.java
@@ -1,11 +1,14 @@
 package src.JavaCollections;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 class Node {
     int data;
     Node next;
 }
 
-public class LinkedListClass {
+public class LinkedListClass extends Node implements Iterable<Integer>  {
     Node head;
     Node tail;
     int size;
@@ -32,12 +35,65 @@ public class LinkedListClass {
         }
     }
 
+    void insertAtEndNode(int val) {
+        Node node = head;
+
+        while ((node.next != null )) {
+            node = node.next;
+        }
+        Node newNode = new Node();
+        newNode.data = val;
+        node.next = newNode;
+        //newNode.next = node.next;
+    }
+
+    void insertAfterNode(int val, int afterNode) {
+        Node node = head;
+        Node prev = null;
+
+        while ((node.next != null ) && (node.data != afterNode)) {
+            prev = node;
+            node = node.next;
+        }
+        Node newNode = new Node();
+        newNode.data = val;
+        prev.next = newNode;
+        newNode.next = node.next;
+    }
+
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            Node node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public Integer next() {
+                Node node = head;
+
+                while (node.hasNext()) {
+                    Integer data = node.v;
+                }
+            }
+    }
+
+
     public static void main(String[] args) {
         LinkedListClass ls = new LinkedListClass();
+        System.out.println("----Insert nodes into linked list-----");
         ls.addNewNode(2);
         ls.addNewNode(3);
         ls.addNewNode(4);
-        //System.out.println(newNode);
         ls.getNode();
+        System.out.println("----Insert node after removing node in linked list-----");
+        ls.insertAfterNode(5, 3);
+        ls.getNode();
+        System.out.println("----Insert node at the end in linked list-----");
+        ls.insertAtEndNode(6);
+        ls.getNode();
+        ls.iterateLinkedList();
     }
 }

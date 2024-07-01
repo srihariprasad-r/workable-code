49da9e278385bf19cace2eeb0149fe5514f65a7c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-02 01:01:22 +0530

    added KVstore changes

--- golang/implementations/kvstore/main.go
+++ golang/implementations/kvstore/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Store[K comparable, V any] interface {
 	Put(K, V) error
 	Get(K) ([]V, error)
@@ -10,19 +12,34 @@ type KVStore[K comparable, V any] struct {
 	data map[K]V
 }
 
-type Block struct {}
-type Transaction struct {}
+type Block struct{}
+type Transaction struct{}
 
 func NewKVStore[K comparable, V any]() *KVStore[K, V] {
-	return &KVStore[K,V]{
-		data : make(map[K]V),
+	return &KVStore[K, V]{
+		data: make(map[K]V),
 	}
 }
 
-
-func main() {
-	_  = NewKVStore[string, *Block]()
-	_ = NewKVStore[string, *Transaction]()
+func (s *KVStore[K, V]) Put(key K, value V) error {
+	s.data[key] = value
+	return nil
 }
 
+func (s *KVStore[K, V]) Get(key K) (V, error) {
+	val, err := s.data[key]
+	if !err {
+		return val, fmt.Errorf("No key found")
+	}
+	return val, nil
+}
 
+func main() {
+	//_  = NewKVStore[string, *Block]()
+	//_ = NewKVStore[string, *Transaction]()
+	//var c Store[string, string]
+	store := NewKVStore[string, string]()
+	store.Put("foo", "bar")
+
+	fmt.Println(store.Get("foo"))
+}

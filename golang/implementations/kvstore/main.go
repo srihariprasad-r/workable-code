9141f2cc0ad40f475a56de807ee7851803c7aaca
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-02 18:03:36 +0530

    added KVstore changes

--- golang/implementations/kvstore/main.go
+++ golang/implementations/kvstore/main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Store[K comparable, V any] interface {
 	Put(K, V) error
-	Get(K) ([]V, error)
-	Update(K, V) (K, V)
+	Get(K) (V, error)
+	Update(K, V) (V, error)
 }
 
 type KVStore[K comparable, V any] struct {
 	data map[K]V
+}
+
+type Server struct {
+	store Store[string, string]
+	ListenAddr string
 }
 
 type Block struct{}
@@ -21,8 +29,20 @@ func NewKVStore[K comparable, V any]() *KVStore[K, V] {
 	}
 }
 
+func (s *KVStore[K, V]) Update(key K, value V) (V, error) {
+	val, err := s.Get(key)
+	if err == nil {
+		s.data[key] = value
+		fmt.Println("Old value for : " , key , " is " , val, "  which has been updated to: " , s.data[key])
+		return val, nil
+	} else {
+		return val, err
+	}
+}
+
 func (s *KVStore[K, V]) Put(key K, value V) error {
 	s.data[key] = value
+	fmt.Println(s.data)
 	return nil
 }
 
@@ -34,12 +54,32 @@ func (s *KVStore[K, V]) Get(key K) (V, error) {
 	return val, nil
 }
 
+func NewServer(address string) *Server {
+	return &Server{
+		store: NewKVStore[string, string](),
+		ListenAddr: address,
+	}
+}
+
+func (s *Server) Start() {
+	http.ListenAndServe(s.ListenAddr, nil)
+}
+
 func main() {
 	//_  = NewKVStore[string, *Block]()
 	//_ = NewKVStore[string, *Transaction]()
 	//var c Store[string, string]
-	store := NewKVStore[string, string]()
-	store.Put("foo", "bar")
+	// store := NewKVStore[string, string]()
+	// store.Put("foo", "bar")
 
-	fmt.Println(store.Get("foo"))
+	// fmt.Println(store.Get("foo"))
+
+	s := Server {
+		store: NewKVStore[string, string](),
+	}
+
+	s.store.Put("foo", "bar")
+	fmt.Println(s.store.Get("foo"))
+	// s := NewServer(":3000")
+	// s.Start()
 }

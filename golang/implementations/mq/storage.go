b559f338a2294a4baf70e485d212d0a25d399e53
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-02 23:11:47 +0530

    added KVstore changes

---
+++ golang/implementations/mq/storage.go
@@ -0,0 +1,33 @@
+package storage
+
+import "fmt"
+
+type Store interface {
+	Put([]byte) (int, error)
+	Fetch(int) ([]byte, error)
+}
+
+type MemoryStore struct {
+	data [][]byte
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make([][]byte, 0),
+	}
+}
+
+func (s *MemoryStore) Put(d []byte) (int, error) {
+	s.data = append(s.data, d)
+	return len(s.data) - 1, nil
+}
+
+func (s *MemoryStore) Fetch(offset int) ([]byte, error) {
+	if len(s.data) < offset {
+		return nil, fmt.Errorf("Offset is too large")
+	} 
+	var c []byte
+	copy(c, s.data[offset])
+	return c, nil
+}
+
